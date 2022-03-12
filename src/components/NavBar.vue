79020aab90c87e615965df9e5f739c24bca98c71
selorcav <se.lorcav@gmail.com>
2022-03-12 14:25:19 -0300

    corrige navbar responsive

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -11,11 +11,10 @@
         <a href="#services"><v-btn class="primary-bg open-sans" elevation="0">Servicios</v-btn></a>
         <a href="#aboutus"><v-btn class="primary-bg open-sans" elevation="0">Nosotros</v-btn></a>
         <a href="#contact"><v-btn class="primary-bg open-sans" elevation="0">Contacto</v-btn></a>
-
       </div>
     </v-app-bar>
 
-    <v-navigation-drawer class="primary-bg" dark v-model="drawer" absolute temporary>
+    <v-navigation-drawer class="primary-bg" dark v-model="drawer" fixed temporary>
       <v-list nav dense>
         <v-list-item-group
           v-model="group"
@@ -24,19 +23,19 @@
             <v-img width="181" src="../assets/logos-04.png" />
           </div>
           <v-list-item>
-            <v-list-item-title class="body-1 px-5 open-sans" @click="drawer = !drawer">Inicio</v-list-item-title>
+            <v-list-item-title class="body-1 px-5 open-sans" @click="drawer = !drawer"><a class="white--text" href="#home">Inicio</a></v-list-item-title>
           </v-list-item>
 
           <v-list-item>
-            <v-list-item-title class="body-1 px-5 open-sans" @click="drawer = !drawer">Servicios</v-list-item-title>
+            <v-list-item-title class="body-1 px-5 open-sans" @click="drawer = !drawer"><a class="white--text" href="#services">Servicios</a></v-list-item-title>
           </v-list-item>
 
           <v-list-item>
-            <v-list-item-title class="body-1 px-5 open-sans" @click="drawer = !drawer">Nosotros</v-list-item-title>
+            <v-list-item-title class="body-1 px-5 open-sans" @click="drawer = !drawer"><a class="white--text" href="#aboutus">Nosotros</a></v-list-item-title>
           </v-list-item>
 
           <v-list-item>
-            <v-list-item-title class="body-1 px-5 open-sans" @click="drawer = !drawer">Contacto</v-list-item-title>
+            <v-list-item-title class="body-1 px-5 open-sans" @click="drawer = !drawer"><a class="white--text" href="#contact">Contacto</a></v-list-item-title>
           </v-list-item>
         </v-list-item-group>
       </v-list>
@@ -59,4 +58,6 @@ export default {
   font-family: 'Open Sans', sans-serif !important
 
 }
+
+
 </style>
